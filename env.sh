15c6221bb53e02975632c946d99be27411e66667
simon-poon <93788837+simon-poon@users.noreply.github.com>
2023-04-16 01:34:13 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export DATADIR_JetClass=/mettransformervol/JetClass-data/JetClass
-export DATADIR_TopLandscape=
+export DATADIR_TopLandscape=/mettransformervol/QuarkArrow-data/QuarkArrow
 export DATADIR_QuarkGluon=
