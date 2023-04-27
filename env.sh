785d5cc1019d48f0490c7df6b3d111b5a7d8cea4
simon-poon <93788837+simon-poon@users.noreply.github.com>
2023-04-27 13:07:38 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export DATADIR_JetClass=/mettransformervol/JetClass-data/JetClass
+export DATADIR_JetClass=/mettransformervol/perfNano_TTbar_PU200.110X
 export DATADIR_TopLandscape=
 export DATADIR_QuarkGluon=/mettransformervol/QuarkGluon-data/QuarkGluon
